00c4f84a162ba0c1b4395a4cebb40527f8dccac4
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-05-02 12:23:23 +0900

    map 함수로 리스트 구현

--- fe-session5-assignment/src/App.js
+++ fe-session5-assignment/src/App.js
@@ -8,16 +8,29 @@ function App() {
     { id: 3, info: { name: "오렌지", price: 2000 } },
     { id: 4, info: { name: "포도", price: 2500 } },
   ];
-  
+
   //각 아이템 클릭 시 알림핸들러
-  const handleClick=(fruitname)=>{
+  const handleClick = (fruitname) => {
     alert(fruitname);
-  }
+  };
   return (
     <div>
       <h2>상품 목록</h2>
       <ul>
-       
+        {fruits.map(//map함수로 반복적인 요소 구현
+          (
+            fruit 
+          ) => (
+            <li
+              key={fruit.id}
+              onClick={() => {
+                handleClick(fruit.info.name);
+              }}
+            >
+              {" "}{fruit.info.name} - {fruit.info.price}원{" "}
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
