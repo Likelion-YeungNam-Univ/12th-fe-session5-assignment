f5162ffd05949f5edea1b5d52b5ca68ed7425d7a
Yeongi Park <yeongi0111@naver.com>
2024-05-02 13:34:38 +0900

    과제 완료

--- homework5/src/App.js
+++ homework5/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const products = [
+    { id: 1, info: { name: "사과", price: 1000 } },
+    { id: 2, info: { name: "바나나", price: 1500 } },
+    { id: 3, info: { name: "오렌지", price: 2000 } },
+    { id: 4, info: { name: "포도", price: 2500 } },
+  ];
+
+  const onClick = (name) => {
+    alert(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>상품 목록</h1>
+      <ul>
+        {products.map((item) => (
+          <li id={item.id}>
+            <span
+              onClick={() => {
+                onClick(item.info.name);
+              }}
+            >
+              <span id="name">{item.info.name}</span> - {item.info.price}원
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
