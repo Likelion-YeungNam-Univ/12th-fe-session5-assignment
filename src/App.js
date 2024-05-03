a1dc4eba147943790a343985ed039a1e8a153855
jihyun132 <jihyun.q.s@gmail.com>
2024-05-03 15:33:10 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <React.Fragment>
+      <ProductList/>
+    
+    </React.Fragment>
+  );
+}
+
+export default App;
