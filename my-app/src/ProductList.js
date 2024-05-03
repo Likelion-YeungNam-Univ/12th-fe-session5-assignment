573df4da914763a7b7b48459cc82de0e543b84ce
jihyun132 <jihyun.q.s@gmail.com>
2024-05-03 16:39:26 +0900

    파일 정리

---
+++ my-app/src/ProductList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function ProductList() {
+   // 배열 데이터
+   const products = [
+    { id: 1, info: { name: "사과", price: 1000 } },
+    { id: 2, info: { name: "바나나", price: 1500 } },
+    { id: 3, info: { name: "오렌지", price: 2000 } },
+    { id: 4, info: { name: "포도", price: 2500 } },
+  ];
+
+  const elements = products.map((product)=>(
+    <li 
+    onClick={() => {
+      alertName(product.info.name);
+    }}
+    key={product.id}>{product.info.name} - {product.info.price}원</li>
+  ));
+  return (
+    <div>
+      <h1>상품 목록</h1>
+      <ul>{elements}</ul>
+    </div>
+  );
+}
+
+const alertName = (a) => {
+  alert(a);
+}
+
+export default ProductList;
