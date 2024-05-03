9dbe8e5244a6b6282a8af5b92d4d719e67f06f36
minseo <choms1357@gmail.com>
2024-05-03 17:42:15 +0900

    5회차 과제

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function List(){
+  const products = [
+    { id: 1, info: { name: "사과", price: 1000 } },
+    { id: 2, info: { name: "바나나", price: 1500 } },
+    { id: 3, info: { name: "오렌지", price: 2000 } },
+    { id: 4, info: { name: "포도", price: 2500 } },
+  ];
+  
+  const click = (listItem) => {
+    alert(listItem);
+  }
+
+  const lists = products.map((list)=>
+    <ul>
+      <li key="list.id" onClick={() => click(list.info.name)}><span>{list.info.name}</span> - {list.info.price}원</li>
+    </ul>
+  )
+
+  return (
+    lists
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>상품목록</h1>
+      <List></List>
+    </div>
+  );
+}
+
+export default App;
