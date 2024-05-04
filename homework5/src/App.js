833343e225d493d9afefef46da3a1403fe74315d
dev-joohee <dev.joohee0916@gmail.com>
2024-05-04 15:25:13 +0900

    5회차과제-김주희

--- homework5/src/App.js
+++ homework5/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+    const products = [
+      { id: 1, info: { name: "사과", price: 1000 } },
+      { id: 2, info: { name: "바나나", price: 1500 } },
+      { id: 3, info: { name: "오렌지", price: 2000 } },
+      { id: 4, info: { name: "포도", price: 2500 } },
+    ];
+const alertName=(product)=>{alert(product.info.name);}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>상품 목록</h1>
+      
+      <ul>
+        {products.map((product) => (
+            <li key={product.id} onClick={() => {alertName(product)}}>
+            {product.info.name} - {product.info.price}원
+            </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
+
 
 export default App;
