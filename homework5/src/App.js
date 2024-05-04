e59584f7e3cf26bb08a3b50942263aececc511b8
Junhyeok Koo <rnwnsgur28@gmail.com>
2024-05-04 22:56:08 +0900

    app.js 구현

---
+++ homework5/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  const products = [
+    { id: 1, info: { name: '사과', price: 1000 } },
+    { id: 2, info: { name: '바나나', price: 1500 } },
+    { id: 3, info: { name: '오렌지', price: 2000 } },
+    { id: 4, info: { name: '포도', price: 2500 } },
+  ];
+
+  return (
+    <div>
+      <h1>상품 목록</h1>
+      <ul>
+        {products.map((product) => (
+          <li
+            onClick={() => {
+              alert(product.info.name);
+            }}
+            key={product.id}
+          >
+            {product.info.name} - {product.info.price}원
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
